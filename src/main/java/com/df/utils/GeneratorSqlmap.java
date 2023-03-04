d06437ed1979f08bfccd40212e19bb4c9f0c294a
Loser <15570237906@139.com>
2023-03-04 11:33:50 +0800

    dev环境代码提交

--- src/main/java/com/df/utils/GeneratorSqlmap.java
+++ src/main/java/com/df/utils/GeneratorSqlmap.java
@@ -21,7 +21,7 @@ public class GeneratorSqlmap {
         boolean overwrite = true;
 
         //根据项目文件的路径更改
-        File configFile = new File("D:\\Code\\Mybatis_Re\\src\\main\\resources\\generatorConfig.xml");
+        File configFile = new File("F:\\code\\mybatis_Re\\src\\main\\resources\\generatorConfig.xml");
         ConfigurationParser cp = new ConfigurationParser(warnings);
         Configuration config = cp.parseConfiguration(configFile);
         DefaultShellCallback callback = new DefaultShellCallback(overwrite);
